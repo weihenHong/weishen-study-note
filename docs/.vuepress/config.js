731e03a619c4aefb0fd1cf6d85f062859288b9da
weishen <286572050@qq.com>
2022-08-16 19:24:27 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  title: "Vim Study Notes",
+  description: "Just playing around",
+  themeConfig: {
+    nav: [{ text: "VIM", link: "/VIM/" }],
+    // displayAllHeaders: true,
+    // sidebar: [
+    //     {
+    //       title: 'Group 1',   // 必要的
+    //       path: '/VIM/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+    //       collapsable: false, // 可选的, 默认值是 true,
+    //       sidebarDepth: 2,    // 可选的, 默认值是 1
+    //       children: [
+    //        { 
+    //         title: 'page1',   // 必要的
+    //         path: '/page1.md',
+    //        }
+    //       ]
+    //     },
+       
+    //   ]
+    sidebar: 'auto'
+  },
+};
